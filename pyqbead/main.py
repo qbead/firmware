bddd871beb17a276de26365b2328f15c75434f69
Mark van Beusekom <mvanbeusekom1@tudelft.nl>
2024-07-15 18:27:25 +0200

    bt py stuff

---
+++ pyqbead/main.py
@@ -0,0 +1,51 @@
+import asyncio
+from bleak import BleakScanner, BleakClient
+import bleak
+import bleak.backends
+import bleak.backends.device
+import bleak.backends.service
+
+serviceUuid = "e30c1fc6-359c-12be-2544-63d6aa088d45";
+
+accUuid = "e30c1fc9-359c-12be-2544-63d6aa088d45";
+sphUuid = "e30c1fc8-359c-12be-2544-63d6aa088d45";
+colUuid = "e30c1fc7-359c-12be-2544-63d6aa088d45";
+
+async def discover():
+    devices = await BleakScanner.discover(return_adv=True)
+    for device, adv in devices.values():
+        if device.name is not None and "qbead" in device.name:
+            print(f'Found device: {device}, RSSI: {adv.rssi}')
+    return devices
+
+def selectClosestDevice(devices):
+    max_rssi = 0
+    closest_device = None
+    for device,adv in devices.values():
+        if device.name is not None and "qbead" in device.name and adv.rssi < max_rssi:
+            max_rssi = adv.rssi
+            closest_device = device
+    return closest_device
+
+async def ConnectToDevice(device):
+    print(f'Connecting to device: {device}')
+    assert type(device) is bleak.backends.device.BLEDevice
+    address = device.address
+    async with BleakClient(address) as client:
+        services = await client.services
+        print("Services:")
+        for service in services:
+            print(service)
+        service = services.get_service(serviceUuid)
+        print('Characteristics:')
+        for characteristic in service.characteristics:
+            print(characteristic)
+
+async def main():
+    devices = await discover()
+    closest_device = selectClosestDevice(devices)
+    print(f'Closest device: {closest_device}')
+    await ConnectToDevice(closest_device)
+
+asyncio.run(main())
+
