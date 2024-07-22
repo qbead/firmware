45eabb2469f45c577b55d4228526d7e6bc33d98f
Elena Volkova <venalolkova@gmail.com>
2024-07-22 19:30:14 +0200

    implemented gesture detector for rotation

--- qbeadfirmware/qbeadfirmware.ino
+++ qbeadfirmware/qbeadfirmware.ino
@@ -31,6 +31,18 @@ const uint8_t QB_UUID_ACC_CHAR[] =
 
 const uint8_t zerobuffer20[] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
 
+bool shaking = false;
+bool vectorinit = false;
+bool rotation_z = false;
+bool rotation_x = false;
+bool rotation_y = false;
+unsigned long startshakingTime = 0;
+unsigned long startinitTime = 0;
+unsigned long startrotationTime = 0;
+const unsigned long shakeDuration = 500000; 
+const unsigned long initDuration = 5000000; 
+const unsigned long rotationDuration = 5000000; 
+
 // TODO manage namespaces better
 static uint32_t color(uint8_t r, uint8_t g, uint8_t b) {
   return ((uint32_t)r << 16) | ((uint32_t)g << 8) | b;
@@ -116,20 +128,20 @@ public:
       phi_quant(360 / nlegs),
       ix(ix), iy(iy), iz(iz),
       sx(sx), sy(sy), sz(sz),
-      bleservice(QB_UUID_SERVICE),
-      blecharcol(QB_UUID_COL_CHAR),
-      blecharsph(QB_UUID_SPH_CHAR),
-      blecharacc(QB_UUID_ACC_CHAR)
+     bleservice(QB_UUID_SERVICE),
+     blecharcol(QB_UUID_COL_CHAR),
+     blecharsph(QB_UUID_SPH_CHAR),
+     blecharacc(QB_UUID_ACC_CHAR)
       { 
   };
 
   LSM6DS3 imu;
   Adafruit_NeoPixel pixels;
 
-  BLEService bleservice;
-  BLECharacteristic blecharcol;
-  BLECharacteristic blecharsph;
-  BLECharacteristic blecharacc;
+ BLEService bleservice;
+ BLECharacteristic blecharcol;
+ BLECharacteristic blecharsph;
+ BLECharacteristic blecharacc;
   uint8_t connection_count = 0;
 
   const uint8_t nsections;
@@ -158,28 +170,28 @@ public:
         Serial.println("IMU OK");
     }
 
-    Bluefruit.begin(QB_MAX_PRPH_CONNECTION, 0);
-    Bluefruit.setName("qbead | " __DATE__ " " __TIME__);
-    bleservice.begin();
-    blecharcol.setProperties(CHR_PROPS_READ | CHR_PROPS_WRITE);
-    blecharcol.setPermission(SECMODE_OPEN, SECMODE_OPEN);
-    blecharcol.setUserDescriptor("rgb color");
-    blecharcol.setFixedLen(3);
-    blecharcol.begin();
-    blecharcol.write(zerobuffer20, 3);
-    blecharsph.setProperties(CHR_PROPS_READ | CHR_PROPS_WRITE);
-    blecharsph.setPermission(SECMODE_OPEN, SECMODE_OPEN);
-    blecharsph.setUserDescriptor("spherical coordinates");
-    blecharsph.setFixedLen(2);
-    blecharsph.begin();
-    blecharsph.write(zerobuffer20, 2);
-    blecharacc.setProperties(CHR_PROPS_READ | CHR_PROPS_NOTIFY);
-    blecharacc.setPermission(SECMODE_OPEN, SECMODE_OPEN);
-    blecharacc.setUserDescriptor("xyz acceleration");
-    blecharacc.setFixedLen(3*sizeof(float));
-    blecharacc.begin();
-    blecharacc.write(zerobuffer20, 3*sizeof(float));
-    startBLEadv();
+   Bluefruit.begin(QB_MAX_PRPH_CONNECTION, 0);
+   Bluefruit.setName("qbead | " __DATE__ " " __TIME__);
+   bleservice.begin();
+   blecharcol.setProperties(CHR_PROPS_READ | CHR_PROPS_WRITE);
+   blecharcol.setPermission(SECMODE_OPEN, SECMODE_OPEN);
+   blecharcol.setUserDescriptor("rgb color");
+   blecharcol.setFixedLen(3);
+   blecharcol.begin();
+   blecharcol.write(zerobuffer20, 3);
+   blecharsph.setProperties(CHR_PROPS_READ | CHR_PROPS_WRITE);
+   blecharsph.setPermission(SECMODE_OPEN, SECMODE_OPEN);
+   blecharsph.setUserDescriptor("spherical coordinates");
+   blecharsph.setFixedLen(2);
+   blecharsph.begin();
+   blecharsph.write(zerobuffer20, 2);
+   blecharacc.setProperties(CHR_PROPS_READ | CHR_PROPS_NOTIFY);
+   blecharacc.setPermission(SECMODE_OPEN, SECMODE_OPEN);
+   blecharacc.setUserDescriptor("xyz acceleration");
+   blecharacc.setFixedLen(3*sizeof(float));
+   blecharacc.begin();
+   blecharacc.write(zerobuffer20, 3*sizeof(float));
+   startBLEadv();
   }
 
   void clear() {
@@ -238,21 +250,115 @@ public:
     int phi_int = phi_leg + 0.5;
     phi_int = phi_int > nlegs - 1 ? 0 : phi_int;
 
-    float p = (theta_section - theta_int);
-    int theta_direction = sign(p);
-    p = abs(p);
-    float q = 1 - p;
-    p = p * p;
-    q = q * q;
+    float p_theta = (theta_section - theta_int);
+    int theta_direction = sign(p_theta);
+    float p_phi = (phi_leg - phi_int);
+    int phi_direction = sign(p_phi);
+    p_theta = abs(p_theta);
+    p_phi = abs(p_phi);
+    float q_theta = 1 - p_theta;
+    float q_phi = 1 - p_phi;
+    p_phi = p_phi * p_phi;
+    p_theta = p_theta * p_theta;
+    q_phi = q_phi * q_phi;
+    q_theta = q_theta * q_theta;
 
     uint8_t rc = redch(c);
     uint8_t bc = bluech(c);
     uint8_t gc = greench(c);
 
-    setLegPixelColor(phi_int, theta_int, color(q * rc, q * bc, q * gc));
-    setLegPixelColor(phi_int, theta_int + theta_direction, color(p * rc, p * bc, p * gc));
+    setLegPixelColor(phi_int, theta_int, color(q_phi*q_theta * rc, q_phi*q_theta * bc, q_phi*q_theta * gc));
+    setLegPixelColor(phi_int, theta_int + theta_direction, color(p_theta*q_phi * rc, p_theta*q_phi * bc, p_theta*q_phi * gc));
+    setLegPixelColor(phi_int+phi_direction, theta_int, color(q_theta*p_phi * rc, q_theta*p_phi * bc, q_theta*p_phi * gc));
+    setLegPixelColor(phi_int+phi_direction, theta_int + theta_direction, color(p_theta*p_phi * rc, p_theta*p_phi * bc, p_theta*p_phi * gc));
   }
+  
+  void lightEverythingUp(uint32_t c){
+  for (int i = 0; i < pixels.numPixels(); i++) {
+    pixels.setPixelColor(i, c);
+    show();
+  }
+  delay(2000);
+  clear();
+  show();
+  }
+  
+  void vectorInit(float theta, float phi){
+    setBloch_deg_smooth(theta, phi, color(255, 0, 255));
+    if ((x * x + y * y + z * z) > 1.07) {
+      if (!shaking) {
+        shaking = true;
+        startshakingTime = micros();
+      } else {
+        if (micros() - startshakingTime >= shakeDuration) {
+          uint32_t c = color(0, 255, 0);
+          lightEverythingUp(c);
+          startinitTime = micros();
+          while (micros()-startinitTime<=initDuration)
+          {
+            clear();
+            readIMU();
+            setBloch_deg_smooth(t, p, color(0, 0, 255));
+            show();
+            delay(10);
+            theta = t;
+            phi = p;
+          }
+          delay(5000);
+          clear();
+          setBloch_deg_smooth(theta, phi, color(0, 255, 0)); //fix the necessary vector
+          setLegPixelColor(1, 3, color(255, 255, 255));
+          setLegPixelColor(4, 3, color(255, 255, 255));
+          setLegPixelColor(7, 3, color(255, 255, 255));
+          setLegPixelColor(10, 3, color(255, 255, 255));
+          setLegPixelColor(1, 0, color(255, 255, 255));
+          setLegPixelColor(1, 6, color(255, 255, 255));
+          
+          show();
+          delay(1000);
+          shaking = false;
+          startrotationTime = micros();
+          while (micros()-startrotationTime<=rotationDuration)
+          {
+            Serial.print("\nROTATION PERFORMANCE\n");
+            readIMU();
+            if (0.9<x && x<1.1) 
+            {
+              rotation_z = true;
+            }
+            else
+            {
+              rotation_z = false;
+            }
+            
+          }
+          if(rotation_z)
+          {
+            clear();
+            for (int i=0; i<nsections; i++)
+            {  
+              Serial.print("Rotation over Z is completed");
+              setLegPixelColor(4, i, color(0, 255, 0));
+              setLegPixelColor(10, i, color(0, 255, 0));
+              Serial.print("Rotation over Z is completed\n");
+              delay(300);
+              show();
+            }
+            delay(5000);
+            clear();
+            setBloch_deg(theta+180, phi, color(0, 0, 255));
+            show();
+            delay(20000);
+          }
+        }
+      }
+    } else {
+    shaking = false;
+    }
 
+    delay(10);
+  }
+  
   void readIMU() {
     rbuffer[0] = imu.readFloatAccelX();
     rbuffer[1] = imu.readFloatAccelY();
@@ -285,6 +391,10 @@ public:
     Serial.print(y);
     Serial.print("\t");
     Serial.print(z);
+    Serial.print("\t");
+    Serial.print("SQUARE VALUE:");
+    Serial.print(x*x+y*y+z*z);
+    Serial.print("\t");
     Serial.print("\t-1\t1\t");
     Serial.print(t);
     Serial.print("\t");
@@ -295,43 +405,43 @@ public:
     rbuffer[0] = x;
     rbuffer[1] = y;
     rbuffer[2] = z;
-    blecharacc.write(rbuffer, 3*sizeof(float));
-    for (uint16_t conn_hdl=0; conn_hdl < QB_MAX_PRPH_CONNECTION; conn_hdl++)
-    {
-      if ( Bluefruit.connected(conn_hdl) && blecharacc.notifyEnabled(conn_hdl) )
-      {
-        blecharacc.notify(rbuffer, 3*sizeof(float));
-      }
-    }
+   blecharacc.write(rbuffer, 3*sizeof(float));
+   for (uint16_t conn_hdl=0; conn_hdl < QB_MAX_PRPH_CONNECTION; conn_hdl++)
+   {
+     if ( Bluefruit.connected(conn_hdl) && blecharacc.notifyEnabled(conn_hdl) )
+     {
+       blecharacc.notify(rbuffer, 3*sizeof(float));
+     }
+   }
   }
 
-  void startBLEadv(void)
-  {
-    // Advertising packet
-    Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
-    Bluefruit.Advertising.addTxPower();
+ void startBLEadv(void)
+ {
+   // Advertising packet
+   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+   Bluefruit.Advertising.addTxPower();
 
-    // Include HRM Service UUID
-    Bluefruit.Advertising.addService(bleservice);
+   // Include HRM Service UUID
+   Bluefruit.Advertising.addService(bleservice);
 
-    // Secondary Scan Response packet (optional)
-    // Since there is no room for 'Name' in Advertising packet
-    Bluefruit.ScanResponse.addName();
-    
-    /* Start Advertising
-    * - Enable auto advertising if disconnected
-    * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
-    * - Timeout for fast mode is 30 seconds
-    * - Start(timeout) with timeout = 0 will advertise forever (until connected)
-    * 
-    * For recommended advertising interval
-    * https://developer.apple.com/library/content/qa/qa1931/_index.html   
-    */
-    Bluefruit.Advertising.restartOnDisconnect(true);
-    Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
-    Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
-    Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds  
-  }
+   // Secondary Scan Response packet (optional)
+   // Since there is no room for 'Name' in Advertising packet
+   Bluefruit.ScanResponse.addName();
+//    
+//    /* Start Advertising
+//    * - Enable auto advertising if disconnected
+//    * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+//    * - Timeout for fast mode is 30 seconds
+//    * - Start(timeout) with timeout = 0 will advertise forever (until connected)
+//    * 
+//    * For recommended advertising interval
+//    * https://developer.apple.com/library/content/qa/qa1931/_index.html   
+//    */
+   Bluefruit.Advertising.restartOnDisconnect(true);
+   Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
+   Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
+   Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
+ }
 
 }; // end class
 } // end namespace
@@ -366,7 +476,8 @@ void loop() {
   bead.readIMU();
 
   bead.clear();
-  bead.setBloch_deg_smooth(bead.t, bead.p, color(255,0,255));
+//  bead.setBloch_deg_smooth(bead.t, bead.p, color(255,0,255));
+  bead.vectorInit(bead.t, bead.p);
   bead.show();
   delay(10);
 }
