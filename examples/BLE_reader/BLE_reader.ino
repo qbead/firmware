08cb0c8a065bd9f43e668bde4f2938461531eea1
johanhvdb <78906149+johanhvdb@users.noreply.github.com>
2024-10-21 23:09:26 +0200

    Add files via upload

--- examples/BLE_reader/BLE_reader.ino
+++ examples/BLE_reader/BLE_reader.ino
@@ -1,10 +1,10 @@
-#include <Qbead.h>
+#include <C:\Users\johan\vismult\firmware\src\Qbead.h>
 
 Qbead::Qbead bead;
 
 void setup() {
   bead.begin();
-  bead.setBrightness(25); // way too bright
+  bead.setBrightness(25);  // way too bright
   Serial.println("testing all pixels discretely");
   for (int i = 0; i < bead.pixels.numPixels(); i++) {
     bead.pixels.setPixelColor(i, color(255, 255, 255));
@@ -23,7 +23,16 @@ void setup() {
 }
 
 void loop() {
+  bead.readIMU();
   bead.clear();
-  bead.setBloch_deg_smooth(bead.t_ble, bead.p_ble, bead.c_ble);
+  bead.setBloch_deg_smooth(bead.t_acc, bead.p_acc, color(255, 255, 255));
+  bead.setBloch_deg_smooth(90, 0, color(255, 0, 0));
+  bead.setBloch_deg_smooth(90, 45, color(255, 0, 0));
+  bead.setBloch_deg_smooth(90, 90, color(255, 0, 0));
+  bead.setBloch_deg_smooth(90, 135, color(255, 0, 0));
+  bead.setBloch_deg_smooth(90, 180, color(0, 255, 0));
+  bead.setBloch_deg_smooth(90, 225, color(0, 255, 0));
+  bead.setBloch_deg_smooth(90, 270, color(0, 255, 0));
+  bead.setBloch_deg_smooth(90, 315, color(0, 255, 0));
   bead.show();
 }
