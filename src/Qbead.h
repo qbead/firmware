1c4c0125586d2e09753db9a5ea9ef489e1d7b2c4
Ard Geuze <geuzeard@gmail.com>
2025-05-07 12:33:23 +0200

    Implement axis

--- src/Qbead.h
+++ src/Qbead.h
@@ -227,6 +227,11 @@ public:
     stateCoordinates.set(argStateCoordinates.x, argStateCoordinates.y, argStateCoordinates.z);
   }
 
+  Coordinates getCoordinates()
+  {
+    return stateCoordinates;
+  }
+
   int collapse()
   {
     const float theta = stateCoordinates.theta();
@@ -412,11 +417,25 @@ public:
     pixels.setBrightness(b);
   }
 
-  Coordinates getCoordinatesAdjustedForGravity() {
-    return Coordinates(x, y, z);
+  Coordinates getCoordinatesAdjustedForGravity(Coordinates c) {
+    Coordinates adjusted = c.rotateRelativeTo(Coordinates(x, y, z));
+    return adjusted;
   }
 
-  void setLed(state: QuantumState, color: uint32_t) {
+  void setLed(QuantumState state, uint32_t color) {
+    Coordinates adjusted = getCoordinatesAdjustedForGravity(state.getCoordinates());
+    float theta = adjusted.theta();
+    float phi = adjusted.phi();
+    setBloch_deg(theta, phi, color);
+  }
+
+  void setAxis() {
+    setLed(QuantumState(Coordinates(0, 0, 1)), color(255, 0, 0));
+    setLed(QuantumState(Coordinates(0, 0, -1)), color(255, 0, 0));
+    setLed(QuantumState(Coordinates(0, 1, 0)), color(0, 255, 0));
+    setLed(QuantumState(Coordinates(0, -1, 0)), color(0, 255, 0));
+    setLed(QuantumState(Coordinates(1, 0, 0)), color(0, 0, 255));
+    setLed(QuantumState(Coordinates(-1, 0, 0)), color(0, 0, 255));
   }
 
   void setBloch_deg(float theta, float phi, uint32_t color) {
